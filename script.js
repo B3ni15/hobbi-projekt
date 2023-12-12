2792bec572f36d897957ceb54963dccb91f1a67d
B3ni <xbenitmx2020@gmail.com>
2023-12-12 18:19:06 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,36 +1,18 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const pageContent = document.body.textContent.toLowerCase();
-  
-    if (pageContent.includes('ok')) {
-      const email = prompt('Kérlek add meg az e-mail címed:');
-      
-      // Generálj egy kódot
-      const verificationCode = generateCode();
-  
-      // Küldj egy kérést a szervernek az e-mail és kód elküldéséhez
-      sendVerificationEmail(email, verificationCode);
+const textToType = `
+Üdv a weboldalon, felhasználó! Köszöntelek a weboldalon nézz körbe és köszönöm ha ídőt forítasz a weboldalra! Továbbió szép napot!`;
+
+    let currentText = '';
+    let index = 0;
+    const speed = 50;
+
+    function typeText() {
+        document.getElementById('text-container').textContent = currentText;
+        if (index < textToType.length) {
+            currentText += textToType.charAt(index);
+            index++;
+            setTimeout(typeText, speed);
+        }
     }
-  });
-  
-  function generateCode() {
-    return Math.floor(1000 + Math.random() * 9000).toString();
-  }
-  
-  function sendVerificationEmail(email, code) {
-    // Küldj egy XMLHttpRequest kérést a szervernek
-    const xhr = new XMLHttpRequest();
-    const url = 'server.php';
-    const params = `email=${email}&code=${code}`;
-    
-    xhr.open('POST', url, true);
-    xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-    
-    xhr.onreadystatechange = function () {
-      if (xhr.readyState === 4 && xhr.status === 200) {
-        alert('Az ellenőrző kód elküldve az e-mail címedre!');
-      }
-    };
-    
-    xhr.send(params);
-  }
-  
+
+    typeText();
+
